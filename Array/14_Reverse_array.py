6b7023adb4e792e52f4b6061b73760562428e6be
Chandan Sahu <cs154896@gmail.com>
2024-05-15 17:53:07 +0530

    Added another approaches

--- Array/14_Reverse_array.py
+++ Array/14_Reverse_array.py
@@ -10,3 +10,18 @@ def reverseArray2(arr):
         arr1.append(arr[i])
     return arr1
 print(reverseArray2([2,4,5,3,9]))
+
+#Approach 3
+def reverseArray3(A,start,end):
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+    return A
+
+print(reverseArray3([3,4,5,6],0,3))
+
+#Approach 4
+def reverseArray4(arr):
+    return list(reversed(arr))
+print(reverseArray4([1,2,3,4]))
